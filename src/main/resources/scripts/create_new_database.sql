ca6d09a1cb7954ea54d5b49bc93d997a56963180
Vladislav Chunikhin <chunikhin.95@mail.ru>
2018-05-14 20:08:30 +0400

    updating database and deleting this useless triggers)))

--- src/main/resources/scripts/create_new_database.sql
+++ src/main/resources/scripts/create_new_database.sql
@@ -1,24 +1,25 @@
 CREATE TABLE IF NOT EXISTS employees (
   id                SERIAL,
-  last_name         CHARACTER VARYING(255),
-  first_name        CHARACTER VARYING(255),
-  patronymic        CHARACTER VARYING(255),
-  gender            CHARACTER VARYING(125),
-  date_of_birth     DATE,
-  phone_number      CHARACTER VARYING(255),
-  mail              CHARACTER VARYING(255),
-  employment_date   DATE,
+  last_name         CHARACTER VARYING(255) NOT NULL,
+  first_name        CHARACTER VARYING(255) NOT NULL,
+  patronymic        CHARACTER VARYING(255) NOT NULL,
+  gender            CHARACTER VARYING(125) NOT NULL,
+  date_of_birth     DATE                   NOT NULL,
+  phone_number      CHARACTER VARYING(255) NOT NULL,
+  mail              CHARACTER VARYING(255) NOT NULL,
+  employment_date   DATE                   NOT NULL,
   date_of_dismissal DATE,
-  position          CHARACTER VARYING(255),
-  salary            DOUBLE PRECISION,
+  position          CHARACTER VARYING(255) NOT NULL,
+  salary            DOUBLE PRECISION       NOT NULL,
   is_chief          BOOLEAN,
-  CONSTRAINT pk_employees PRIMARY KEY (id)
+  CONSTRAINT pk_employees PRIMARY KEY (id),
+  CONSTRAINT positive_salary CHECK (salary > 8000)
 );
 
 CREATE TABLE IF NOT EXISTS departments (
   id            SERIAL,
-  name          CHARACTER VARYING(255),
-  creation_date DATE,
+  name          CHARACTER VARYING(255) NOT NULL,
+  creation_date DATE                   NOT NULL,
   chief_id      INTEGER,
   parent_id     INTEGER,
   CONSTRAINT pk_departments PRIMARY KEY (id),
@@ -56,98 +57,4 @@ DROP INDEX IF EXISTS employees_department_id;
 CREATE INDEX employees_department_id
   ON employees
   USING BTREE (department_id);
---------------------------------------------------------------------
-CREATE FUNCTION departments_stamp()
-  RETURNS TRIGGER AS
-$BODY$
-BEGIN
-  IF NEW.name IS NULL
-  THEN
-    RAISE EXCEPTION 'name of department cannot be null';
-  END IF;
-  IF NEW.creation_date IS NULL
-  THEN
-    RAISE EXCEPTION 'creation date cannot be null';
-  END IF;
-  IF NEW.parent_id <= 0
-  THEN
-    RAISE EXCEPTION 'parent id can be more than 0';
-  END IF;
-  IF NEW.parent_id > (SELECT max(d.id)
-                      FROM departments d)
-  THEN
-    RAISE EXCEPTION 'current parent id cannot be more than  max parent id';
-  END IF;
-  RETURN NEW;
-END;
-$BODY$ LANGUAGE plpgsql;
-
-CREATE TRIGGER departments_stamp
-BEFORE INSERT OR UPDATE ON departments
-FOR EACH ROW EXECUTE PROCEDURE departments_stamp();
---------------------------------------------------------------------
-CREATE FUNCTION employees_stamp()
-  RETURNS TRIGGER AS
-$BODY$
-BEGIN
-  IF NEW.last_name IS NULL
-  THEN
-    RAISE EXCEPTION 'last name of employee cannot be null';
-  END IF;
-  IF NEW.first_name IS NULL
-  THEN
-    RAISE EXCEPTION 'first name of employee cannot be null';
-  END IF;
-  IF NEW.patronymic IS NULL
-  THEN
-    RAISE EXCEPTION 'patronymic of employee cannot be null';
-  END IF;
-  IF NEW.gender IS NULL
-  THEN
-    RAISE EXCEPTION 'gender of employee cannot be null';
-  END IF;
-  IF NEW.date_of_birth IS NULL
-  THEN
-    RAISE EXCEPTION 'date of birth of employee cannot be null';
-  END IF;
-  IF NEW.phone_number IS NULL
-  THEN
-    RAISE EXCEPTION 'phone_number of employee cannot be null';
-  END IF;
-  IF NEW.mail IS NULL
-  THEN
-    RAISE EXCEPTION 'mail of employee cannot be null';
-  END IF;
-  IF NEW.employment_date IS NULL
-  THEN
-    RAISE EXCEPTION 'employment date cannot be null';
-  END IF;
-  IF NEW.position IS NULL
-  THEN
-    RAISE EXCEPTION 'position cannot be null';
-  END IF;
-  IF NEW.salary IS NULL
-  THEN
-    RAISE EXCEPTION 'salary cannot be null';
-  END IF;
-  IF NEW.salary <= 0
-  THEN
-    RAISE EXCEPTION 'salary cannot be more than 0';
-  END IF;
-  IF NEW.department_id <= 0
-  THEN
-    RAISE EXCEPTION 'department id can be more than 0';
-  END IF;
-  IF NEW.department_id > (SELECT max(d.id)
-                          FROM departments d)
-  THEN
-    RAISE EXCEPTION 'current department id cannot be more than  max id';
-  END IF;
-  RETURN NEW;
-END;
-$BODY$ LANGUAGE plpgsql;
-
-CREATE TRIGGER employees_stamp
-BEFORE INSERT OR UPDATE ON employees
-FOR EACH ROW EXECUTE PROCEDURE employees_stamp();
 --------------------------------------------------------------------
